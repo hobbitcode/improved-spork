e6954c21ec24ba7c1a8364280c76e9dc23217af1
Robin Paolini <robit26@gmail.com>
2019-09-19 16:50:54 -0700

    Adding a hello option for my iFrame demo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container fluid>
+    <v-btn block @click="testHello">Hello Message</v-btn>
     <v-btn block @click="testSuccess">Success</v-btn>
     <v-btn block @click="testCancel">Cancel</v-btn>
   </v-container>
@@ -10,6 +11,9 @@ export default {
   data: () => ({
   }),
   methods: {
+    testHello () {
+      window.top.postMessage('hello', '*')
+    },
     testSuccess () {
       window.top.postMessage('success', '*')
     },
