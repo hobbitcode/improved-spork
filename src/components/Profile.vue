760f4973b9b479a0745bb6d2b99f08d9f63d4596
Robin Paolini <robit26@gmail.com>
2019-09-18 17:07:57 -0700

    init

---
+++ src/components/Profile.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-container>
+        <v-layout>
+            <v-col cols="12" sm="6" md="3">
+              <v-text-field
+                label="Outlined"
+                outlined
+                v-model="firstName"
+              ></v-text-field>
+            </v-col>
+        </v-layout>
+        <v-layout>
+            <v-col cols="12" sm="6" md="3">
+                <v-btn block @click="submitProfile">Button</v-btn>
+            </v-col>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+      data: () => ({
+        firstName: ''
+      }),
+      watch: {
+        firstName () {
+            if (this.firstName.length > 4) {
+                alert('Too Long!')
+            }
+        }
+      },
+      methods: {
+        submitProfile () {
+            alert('Submitted: ' + this.firstName)
+        }
+      },
+      created: function () {
+        window.scrollTo(0,0)
+      }
+    }
+</script>
---
+++ src/views/Account.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Account Page!</h1>
+        <Profile />
+    </div>
+</template>
+
+<script>
+import Profile from '../components/Profile';
+
+export default {
+  components: {
+    Profile,
+  },
+};
+</script>
