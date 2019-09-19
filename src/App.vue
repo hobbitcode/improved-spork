c0e7f1eb5e2a03ae66f95e0779f6e08384a3bd02
Robin Paolini <robit26@gmail.com>
2019-09-19 16:38:54 -0700

    Move selects to App.vue within v-content; demo setup for passing message to parent app
    Jason, ignore the success/cancel buttons. Just using this app to demo my iFrame solution for you all.

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,37 @@
 
     <v-content>
       <router-view/>
+      <v-row align="center">
+        <v-col class="d-flex" cols="12" sm="6">
+          <v-select
+            :items="items"
+            label="Standard"
+          ></v-select>
+        </v-col>
+        <v-col class="d-flex" cols="12" sm="6">
+          <v-select
+            :items="items"
+            filled
+            label="Filled style"
+          ></v-select>
+        </v-col>
+
+        <v-col class="d-flex" cols="12" sm="6">
+          <v-select
+            :items="items"
+            label="Outlined style"
+            outlined
+          ></v-select>
+        </v-col>
+
+        <v-col class="d-flex" cols="12" sm="6">
+          <v-select
+            :items="items"
+            label="Solo field"
+            solo
+          ></v-select>
+        </v-col>
+      </v-row>
     </v-content>
   </v-app>
 </template>
@@ -30,7 +61,7 @@ export default {
     Profile,
   },
   data: () => ({
-    //
+    items: ['Foo', 'Bar', 'Fizz', 'Buzz']
   }),
 }
 </script>
